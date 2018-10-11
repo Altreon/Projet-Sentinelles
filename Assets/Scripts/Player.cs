c6c955217bf221fd6dd942d123816bce62da04c7
altreon <matteo.brandi.pro@gmail.com>
2018-10-11 22:39:22 +0200

    .

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float speed;
+	
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+		
+		Vector3 move = new Vector3(moveH * speed, 0, moveV * speed);
+		
+		rb.velocity = move;
+	}
+}
