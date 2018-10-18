300d55cb8418101c989109fa0ce6faf8dfb81a93
Matteo BRANDI <matteo.brandi@ensiie.fr>
2018-10-18 21:35:53 +0200

    kkk

---
+++ Assets/Scripts/Sentinelle/Eye.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour {
+
+    private float angleXRemaining;
+    private int angleXDir;
+    private float angleYRemaining;
+    private int angleYDir;
+
+	public Player player;
+    public float speed;
+    public float tolerance;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Mouvement de l'oeil
+        if (angleXRemaining - tolerance > 0) {
+            float angleRotate = speed * Time.deltaTime * angleXRemaining;
+            transform.RotateAround(transform.parent.transform.position, transform.up * -1, angleRotate * angleXDir);
+            angleXRemaining -= angleRotate;
+        }
+
+        if (angleYRemaining - tolerance > 0) {
+            float angleRotate = speed * Time.deltaTime * angleYRemaining;
+            transform.RotateAround(transform.parent.transform.position, transform.forward, angleRotate * angleYDir);
+            angleYRemaining -= angleRotate;
+        }
+		
+		//Détéction du joueur
+		if (detectPlayer()){
+			Debug.Log("Game Over");
+		}else{
+			Debug.Log("En vie!");
+		}
+
+    }
+
+    public void MoveEye (float angleX, float angleY) {
+        if (angleX > 0) {
+            angleXDir = 1;
+        }
+        else {
+            angleXDir = -1;
+        }
+        angleXRemaining = angleX * angleXDir;
+
+        if (angleY > 0) {
+            angleYDir = 1;
+        }
+        else {
+            angleYDir = -1;
+        }
+        angleYRemaining = angleY * angleYDir;
+
+    }
+
+    public bool isMoving() {
+        if (angleXRemaining - tolerance > 0 || angleYRemaining - tolerance > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+	private bool detectPlayer () {
+		Debug.DrawLine(Vector3.zero, transform.right * 10, Color.red, 2.5f);
+		Debug.DrawLine(Vector3.zero, player.transform.position - transform.position, Color.green, 2.5f);
+		
+		Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.yellow);
+		if((angleBetweenVector(transform.right, player.transform.position - transform.position)) * (360/Mathf.PI) < 50) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private double angleBetweenVector(Vector3 v1, Vector3 v2){
+		return Mathf.Acos((float)(Vector3.Dot(v1, v2) / (norme(v1) * norme(v2))));
+	}
+	
+	private double norme (Vector3 vect) {
+		return Mathf.Sqrt(Mathf.Pow(vect.x,2) + Mathf.Pow(vect.y,2) + Mathf.Pow(vect.z,2));
+	}
+}
