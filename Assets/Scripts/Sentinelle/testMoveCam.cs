59dbbb15520cbc21d5c310a3cfde9b4379c2e7ff
altreon <matteo.brandi.pro@gmail.com>
2018-10-29 15:25:14 +0100

    cam suit le joueur

--- Assets/Scripts/Sentinelle/testMoveCam.cs
+++ Assets/Scripts/Sentinelle/testMoveCam.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class testMoveCam : MonoBehaviour {
     public Transform posCam;
+    public Transform player;
+    public int distance;
+    public Vector3 rotation;
 
     private Transform[] pos;
-
     private int currentPos;
+
+    private bool follow;
 
 	// Use this for initialization
 	void Start () {
@@ -25,11 +29,29 @@ public class testMoveCam : MonoBehaviour {
                 currentPos = 0;
             }
             changeCamPosition(currentPos);
+            follow = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            camFollowPlayer(distance, rotation);
+            follow = true;
+        }
+
+        if(follow){
+            camFollowPlayer(distance, rotation);
         }
 	}
 
     public void changeCamPosition(int id){
         transform.position = pos[id].position;
         transform.rotation = pos[id].rotation;
-    }   
+    }
+    
+    public void camFollowPlayer (float distance, Vector3 rotation) {
+        transform.position = new Vector3(player.position.x, player.position.y + distance, player.position.z);
+        transform.RotateAround(player.position, Vector3.right, rotation.x);
+        transform.RotateAround(player.position, Vector3.up, rotation.y);
+        transform.RotateAround(player.position, Vector3.forward, rotation.z);
+        transform.LookAt(player);
+    }
 }
